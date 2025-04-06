76b96fb5db2a3a7f8b647405eecb20e2558b7ab1
TorresMariana <mt.grafico@gmail.com>
2025-04-05 21:27:58 -0300

    responsive

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function menuHamburguesa(){
+    document.getElementById('menu-hamburguesa').click();
+    return;
+}
+
+function cerrarMenuHamburguesa() {
+    // Desmarca el checkbox
+    document.getElementById('menu-hamburguesa').checked = false; 
+    return;
+}
